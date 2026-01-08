d237d11d085361d925542ef000c5d8d1fb9b5d22
Paulo Amaral <ppaulo.developer@gmail.com>
2026-01-07 23:53:19 -0300

    .

--- app/Services/MetaConversionService.php
+++ app/Services/MetaConversionService.php
@@ -13,23 +13,27 @@ class MetaConversionService
                 [
                     'event_name' => 'CompleteRegistration',
                     'event_time' => time(),
-                    'event_id' => $eventId, // importante p/ deduplicação
                     'action_source' => 'website',
                     'user_data' => [
                         'em' => hash('sha256', strtolower(trim($user->email))),
                         'client_ip_address' => request()->ip(),
                         'client_user_agent' => request()->userAgent(),
-                    ]
-
+                    ],
                 ]
             ],
+            'test_event_code' => 'TEST18042',
         ];
 
-        $response = Http::post(
-            'https://graph.facebook.com/v24.0/' . env('META_PIXEL_ID') . '/events?access_token=' . env('META_ACCESS_TOKEN'),
-            $payload
-        );
+        $url = 'https://graph.facebook.com/v24.0/' . env('META_PIXEL_ID') . '/events?access_token=' . env('META_ACCESS_TOKEN');
+
+        $response = Http::post($url, $payload);
+
+        logger()->info('META RESPONSE', [
+            'status' => $response->status(),
+            'body' => $response->json(),
+        ]);
 
         return $response->successful();
+
     }
 }
